2c3153cd1b233d4f4941b28463e3659a12d97258
avluu <avluu@student.cs.uwaterloo.ca>
2017-10-01 18:42:04 -0400

    Changed compile to use ASST1 instead of ASST0

--- config_compile.sh
+++ config_compile.sh
@@ -5,8 +5,8 @@
 cd os161-1.99
 ./configure --ostree=$HOME/cs350-os161/root --toolprefix=cs350-
 cd kern/conf
-./config ASST0
-cd ../compile/ASST0
+./config ASST1
+cd ../compile/ASST1
 bmake depend
 bmake
 if [[ $? -ne 0 ]] ; then

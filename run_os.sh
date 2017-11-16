3cbc000dcec30c340e38d994571a0e6eb2102414
avluu <avluu@student.cs.uwaterloo.ca>
2017-11-15 21:24:49 -0500

    Added test programs to run script

--- run_os.sh
+++ run_os.sh
@@ -3,10 +3,16 @@
 # This script is for running the OS
 
 cd ./root/
-# sys161 kernel "p testbin/palin;q"
-# sys161 kernel "p uw-testbin/onefork;q"
+ sys161 kernel "p uw-testbin/onefork;q"
 # sys161 kernel "p uw-testbin/pidcheck;q"
 # sys161 kernel "p uw-testbin/widefork;q"
 # sys161 kernel "p testbin/forktest;q"
-# sys161 -w kernel "p testbin/forktest"
-sys161 kernel
+
+# sys161 -w kernel "p uw-testbin/hogparty"
+# sys161 -w kernel "p testbin/sty"
+# sys161 -w kernel "p uw-testbin/argtesttest"
+# sys161 -w kernel "p testbin/argtest"
+# sys161 -w kernel "p uw-testbin/argtest"
+# sys161 -w kernel "p testbin/add"
+
+# sys161 kernel

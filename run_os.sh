77a79b6d792707f89706706af8ab144435ab6e6c
avluu <avluu@student.cs.uwaterloo.ca>
2017-11-15 21:28:59 -0500

    Fixed test lines for run script

--- run_os.sh
+++ run_os.sh
@@ -3,16 +3,17 @@
 # This script is for running the OS
 
 cd ./root/
- sys161 kernel "p uw-testbin/onefork;q"
+# sys161 kernel "p uw-testbin/onefork;q"
 # sys161 kernel "p uw-testbin/pidcheck;q"
 # sys161 kernel "p uw-testbin/widefork;q"
 # sys161 kernel "p testbin/forktest;q"
 
-# sys161 -w kernel "p uw-testbin/hogparty"
-# sys161 -w kernel "p testbin/sty"
-# sys161 -w kernel "p uw-testbin/argtesttest"
-# sys161 -w kernel "p testbin/argtest"
-# sys161 -w kernel "p uw-testbin/argtest"
-# sys161 -w kernel "p testbin/add"
+# sys161 kernel "p uw-testbin/hogparty;q"
+ sys161 kernel "p testbin/sty;q"
+# sys161 kernel "p uw-testbin/argtesttest;q"
+# sys161 kernel "p testbin/argtest;q"
+# sys161 kernel "p uw-testbin/argtest;q"
+# sys161 kernel "p testbin/add;q"
 
+# sys161 -w kernel
 # sys161 kernel
